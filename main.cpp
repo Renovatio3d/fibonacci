bc72f09a6b04cee7354a5cb0f83fc3b219601cd7
Shallu Nah <renovatio3d@gmail.com>
2020-01-02 20:57:36 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// AUTHOR: Shallu J Nah
+// PROGRAM DESCRIPTION: A basic program,Fibonacci series of integer numbers.
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <cctype>
+
+using namespace std;
+// Description: Main() Reads the first, and second term, 2 functions to displayPrompt() for user and disResult()
+int main() {
+
+    cout << " The first 10 elements of a Fibonacci series" << endl << endl;
+
+    return 0;
+}
